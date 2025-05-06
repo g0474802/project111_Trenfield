4bf635e8814c1c162d2f48e6a9689022bcf05e17
g0474802 <g0474802@amarillocollege.com>
2025-05-05 22:08:55 -0500

    project11-01.js

--- project111_Trenfield/project11-01.js
+++ project111_Trenfield/project11-01.js
@@ -15,18 +15,18 @@ let dateBox = document.getElementById("dateBox");
 let dateStr = dateBox.value;//gets selected date from the input
 
 dateBox.onchange = function() {   
-      fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dateStr}`)
-    .then(response => response.json())
-    .then(json => showPicture(json))
-    .catch(error => console.log("Error:", error));
+      fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dateStr}`)//sends a request to the NASA API 
+    .then(response => response.json())//converts HTTP into JSON format
+    .then(json => showPicture(json))//calls a function showPicture()
+    .catch(error => console.log("Error:", error));//logs any errors 
 };
 
-function showPicture(json) {
-      if (json.media_type === "video") {
+function showPicture(json) {//will display the image or video 
+      if (json.media_type === "video") {//if media is a video, show it inside <iframe>.
         imageBox.innerHTML = `<iframe src="${json.url}"></iframe><h1>${json.title}</h1><p>${json.explanation}</p>`;
-      } else if (json.media_type === "image") {
+      } else if (json.media_type === "image") {//if an image, show it using <img> tag
         imageBox.innerHTML = `<img src="${json.url}"><h1>${json.title}</h1><p>${json.explanation}</p>`;
-      } else {
+      } else {//if it's something else, show “Image not available.”
         imageBox.innerHTML = "Image not available.";
       }
     }
